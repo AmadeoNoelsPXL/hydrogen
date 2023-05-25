64a61f3fdbcad643d4d555623e95caa5ddab220a
AmadeoNoelsPXL <74738649+AmadeoNoelsPXL@users.noreply.github.com>
2023-05-25 12:06:46 +0200

    Update index.ts

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -1,7 +1,1 @@
-import { createStorefrontClient } from "@shopify/hydrogen-react";
 
-export const client = createStorefrontClient({
-  storeDomain: "https://motorstore-2063.myshopify.com",
-  storefrontApiVersion: "2023-04",
-  privateStorefrontToken: "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",
-});
